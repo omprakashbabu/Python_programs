4e696ec0fe128ec4247b197eee4edbaec00452a3
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-13 12:28:43 +0530

    Update checking even and prime number or not.py

--- checking even and prime number or not.py
+++ checking even and prime number or not.py
@@ -2,22 +2,25 @@ def isEven(a):
     if a%2 == 0:
         print("The given number",a,"is an Even number")
     else:
-        print("The given number",a,"is not an Even number")
+        print("The given number",a,"is an Odd number")
 
 def isPrime(a):
-    if a==1:
+    flag=0
+    if a==1 or a==0:
         print(a,"is not a prime number")
-    elif a>1:
+    else:
         for i in range(2,a):
             if(a%i) == 0:
-                print(a,"is not a prime number")
+                flag =1
             else:
-                print(a,"is a prime number")
-                break
-    else:
-        print(a,"is not a prime number")
+                continue
+        if flag==1:
+            print("The given number is not a prime number")
+        else:
+            print("The given number is a prime number")
+                        
+    
     
 a=int(input("Enter a number : "))
-
 even = isEven(a)
 prime = isPrime(a)
