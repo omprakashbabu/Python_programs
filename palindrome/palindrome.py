a1af53e458ece3e8bd1b9197a7b4ff69b49d5195
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-13 09:48:53 +0530

    Create palindrome.py

---
+++ palindrome/palindrome.py
@@ -0,0 +1,11 @@
+
+def isPalindrome(a):
+    if str == str_rev:
+        print("The given string is Palindrome")
+    else:
+        print("The given string is not a Palindrome")
+
+str = input ("Enter a string : ")
+str_rev = str[::-1]
+print("The reverse of the string is",str_rev)
+palindrome = isPalindrome(str)
